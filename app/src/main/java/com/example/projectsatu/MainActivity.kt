642f97bde553402999b8dc9d3f090f8ea04589eb
BaktiarMahardika <baktiarmahardika08@gmail.com>
2020-05-03 21:19:12 +0700

    Data Validation

--- app/src/main/java/com/example/projectsatu/MainActivity.kt
+++ app/src/main/java/com/example/projectsatu/MainActivity.kt
@@ -3,7 +3,15 @@ package com.example.projectsatu
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+
+private var namaInput : String = ""
+private var emailInput : String = ""
+private var telpInput : String = ""
+private var alamatInput : String = ""
+private var genderInput : String = ""
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +19,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSave.setOnClickListener { goToProfilActivity() }
+        btnSave.setOnClickListener { validasiInput() }
+//        btnSave.setOnClickListener { setDataSpinnerGender() }
+    }
+
+    private fun setDataSpinnerGender(){
+        val adapter = ArrayAdapter.createFromResource(this, R.array.jenis_kelamin, android.R.layout.simple_spinner_dropdown_item)
+        spinnerGender.adapter = adapter
+    }
+
+    private fun validasiInput(){
+        namaInput = editName.text.toString()
+        emailInput = editEmail.text.toString()
+        telpInput = editTelp.text.toString()
+        alamatInput = editAddress.text.toString()
+        genderInput = spinnerGender.selectedItem.toString()
+
+        when{
+            namaInput.isEmpty() -> editName.error = "Nama tidak boleh kosong"
+            genderInput.equals("Jenis Kelamin harus dipilih", ignoreCase = true) -> tampilToast("Jenis Kelamin harus dipilih")
+            emailInput.isEmpty() -> editEmail.error = "Email tidak boleh kosong"
+            telpInput.isEmpty() -> editTelp.error = "Telepon tidak boleh kosong"
+            alamatInput.isEmpty() -> editAddress.error = "Alamat tidak boleh kosong"
+
+            else-> {
+                tampilToast("Navigasi ke halaman profil")
+                goToProfilActivity()
+            }
+        }
+    }
+
+    private fun tampilToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     private fun goToProfilActivity() {
