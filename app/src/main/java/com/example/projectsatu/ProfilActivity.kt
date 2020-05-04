f09a2db5dc3ae7ff8d5d648e3f56ca51e022ab1c
BaktiarMahardika <baktiarmahardika08@gmail.com>
2020-05-04 17:48:00 +0700

    Edit Profile and Dial

--- app/src/main/java/com/example/projectsatu/ProfilActivity.kt
+++ app/src/main/java/com/example/projectsatu/ProfilActivity.kt
@@ -1,8 +1,14 @@
 package com.example.projectsatu
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_profil.*
+import android.content.Intent
+import android.net.Uri
+
+import android.widget.Toast
+
 
 class ProfilActivity : AppCompatActivity() {
 
@@ -11,6 +17,9 @@ class ProfilActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profil)
 
         ambilData()
+        btnEditName.setOnClickListener { navigasiKeEditProfil() }
+        btnTelepon.setOnClickListener { dialPhoneNumber(txtTelp.text.toString()) }
+
     }
 
     private fun ambilData(){
@@ -28,4 +37,37 @@ class ProfilActivity : AppCompatActivity() {
         txtTelp.text = telp
         txtAddress.text = alamat
     }
+
+    companion object {
+        val REQUEST_CODE = 100
+    }
+
+    private fun navigasiKeEditProfil() {
+        val intent = Intent(this, EditProfilActivity::class.java)
+        val namaUser = txtname.text.toString()
+        intent.putExtra("nama", namaUser)
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data:
+    Intent?) {
+        if (requestCode == REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK) {
+                val result = data?.getStringExtra("nama")
+                txtname.text = result
+            }else{
+                Toast.makeText(this, "Edit failed",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun dialPhoneNumber(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
 }
